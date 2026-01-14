b6b479d5b47de6cfb8872a54ed5091b2663c8194
BNSA <65733705+BNSA@users.noreply.github.com>
2026-01-14 13:56:32 +0530

    Change greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World from GitHub!");
+        //System.out.println("Hello, World from GitHub!");
+        System.out.println("Hello from Bengaluru! Today is " + new java.util.Date());
     }
 }
